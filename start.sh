c366e70b7ff97f6c23fbcb65aad73a5454aa298d
m0m3nt0 <liadmz@gmail.com>
2020-03-21 14:59:33 +0200

    Create start.sh

---
+++ start.sh
@@ -0,0 +1,16 @@
+
+#!/bin/bash
+
+NGROKCOMMAND="ngrok http https://localhost"
+SLACKWEBHOOK="https://hooks.slack.com/services/<TOKEN>/<TOKEN>/<TOKEN>"
+
+
+echo "Starting nGROCK"
+gnome-terminal -- $NGROKCOMMAND
+sleep 2
+
+
+NGURL=$(curl --silent http://localhost:4040/api/tunnels | jq -r '.tunnels[1].public_url')
+
+
+curl -X POST -H 'Content-type: application/json' --data "{\"text\":\"Access URL is now:\r\n${NGURL}\"}" $SLACKWEBHOOK
